7d15fec2b5afdc9b84246bf25b83c6e6bc73660f
koontan27 <koontan27@gmail.com>
2019-10-03 21:46:28 +0700

    GameFragment LiveData Databinding

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -55,13 +55,8 @@ class GameFragment : Fragment() {
         viewModel.nextWord()
 
         binding.gameViewModel = viewModel
+        binding.lifecycleOwner = this
 
-        viewModel.score.observe(this, Observer { newScore ->
-            binding.scoreText.text = newScore.toString()
-        })
-        viewModel.word.observe(this, Observer { newWord ->
-            binding.wordText.text = newWord
-        })
         viewModel.eventGameFinish.observe(this, Observer<Boolean> { hasFinished ->
             if (hasFinished) gameFinished()
         })
